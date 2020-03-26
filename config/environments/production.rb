20e8b325f4df6d2c911212ca23bfe6b31be13b92
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-03-27 02:09:36 +0900

    fix action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,13 +93,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'https://stormy-bastion-98307.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'stormy-bastion-98307.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    address: "smtp.SendGrid.net",
+    address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
